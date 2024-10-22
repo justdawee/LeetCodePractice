6a3b35abcf931eb78b61738b6c01196690500991
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2024-10-22 20:58:06 +0200

    Uploading the solution for 27_RemoveElement

---
+++ 27_RemoveElement/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _27_RemoveElement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveElement([3,2,2,3], 3));
+    }
+    
+    public static int RemoveElement(int[] nums, int val) 
+    {
+        int i = 0;
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] != val) // if the value is not equal to the target value
+            {
+                nums[i] = nums[j]; // replace the current value with the value at the current index
+                i++;
+            }
+        }
+        return i; // return the length of the new array
+    }
+}
