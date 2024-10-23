d18d8e24cd6267077a79576a7c569ea20f0098d7
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2024-10-23 12:46:30 +0200

    Solution for 26_RemoveDuplicatesSortedArray

---
+++ 26_RemoveDuplicatesSortedArray/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _26_RemoveDuplicatesSortedArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveDuplicates([0,0,1,1,1,2,2,3,3,4]));
+    }
+    
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int i = 0;
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (i == 0 || nums[j] != nums[i - 1]) 
+            {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}
