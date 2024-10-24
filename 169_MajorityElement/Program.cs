31b02f20e9b7ea608f2fc0b93e51648760c1972c
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2024-10-24 11:09:07 +0200

    Solution for 169_MajorityElement

---
+++ 169_MajorityElement/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _169_MajorityElement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MajorityElement([2,2,1,1,1,2,2,3,3,3,3,3]));
+    }
+    
+    public static int MajorityElement(int[] nums)
+    {
+        int count = 0;
+        int majority = 0; 
+        
+        foreach (var num in nums)
+        {
+            if (count == 0)
+            {
+                majority = num;
+            }
+
+            if (num == majority)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return majority;
+    }
+}
