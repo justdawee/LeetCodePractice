f107045f1c11f87c059eb85e201d181cf031352c
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2024-10-24 10:54:55 +0200

    Uploading solution for 80_RemoveDuplicatesSortedArray2.

---
+++ 80_RemoveDuplicatesSortedArray2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _80_RemoveDuplicatesSortedArray2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveDuplicates([1,1,1,2,2,3]));
+        
+    }
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int i = 0;
+        foreach (var num in nums)
+        {
+            if (i < 2 || num > nums[i - 2]) // i < 2 is for the first two elements
+            {
+                nums[i] = num; // replace the element at i with the current element
+                i++; 
+            }
+        }
+        return i;
+    }
+}
